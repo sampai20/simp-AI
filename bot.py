845e8a5e4f3b6c19b2fd7bd07e3524caefb9131e
Sameer Pai <sampai@mit.edu>
2020-11-30 18:06:44 -0500

    added ping on rank change

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 import discord
 import requests
 import os
+import urllib
 from bs4 import BeautifulSoup
 
 token = os.getenv("simp_AI")
 
 def get_rank(name):
-	url = "https://tracker.gg/valorant/profile/riot/" + name.replace("#", "%23") + "/overview?playlist=competitive"
+	url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
 	req = requests.get(url)
 	soup = BeautifulSoup(req.content, "html.parser")
 	ans = ""
@@ -16,16 +17,9 @@ def get_rank(name):
 		ans = "bad"
 	return ans
 
-def get_camel_rank():
-	url = "https://tracker.gg/valorant/profile/riot/camellCase%23NA1/overview?playlist=competitive"
-	req = requests.get(url)
-	soup = BeautifulSoup(req.content, "html.parser")
-	return soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
-
-
-
 
 client = discord.Client()
+camel_rank = get_rank("camellCase#NA1")
 
 @client.event
 async def on_ready():
@@ -37,9 +31,12 @@ async def on_message(message):
         return
 
     if message.content.startswith('$camel'):
-        await message.channel.send(get_rank("camellCase#NA1"))
+    	cur_rank = get_rank("camellCase#NA1")
+    	await message.channel.send(get_rank("camellCase#NA1"))
+    	if cur_rank != camel_rank:
+    		await message.channel.send("<@&781636133762629632>, camel rank has changed to " + cur_rank + "!")
+    		camel_rank = cur_rank
+        
 
     elif message.content.startswith('$rank'):
     	await message.channel.send(get_rank(message.content[6:]))
-
-client.run(token)
