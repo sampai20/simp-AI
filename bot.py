5d8bd094d7f299c2825ef603f7a37044ae77d04e
Sameer Pai <sampai@mit.edu>
2020-11-30 17:25:46 -0500

    added rank lookup

--- bot.py
+++ bot.py
@@ -5,6 +5,17 @@ from bs4 import BeautifulSoup
 
 token = os.getenv("simp_AI")
 
+def get_rank(name):
+	url = "https://tracker.gg/valorant/profile/riot/" + name.replace("#", "%23") + "/overview"
+	req = requests.get(url)
+	soup = BeautifulSoup(req.content, "html.parser")
+	ans = ""
+	try:
+		ans = soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
+	except:
+		ans = "bad"
+	return ans
+
 def get_camel_rank():
 	url = "https://tracker.gg/valorant/profile/riot/camellCase%23NA1/overview"
 	req = requests.get(url)
@@ -26,7 +37,9 @@ async def on_message(message):
         return
 
     if message.content.startswith('$camel'):
-        await message.channel.send(get_camel_rank())
+        await message.channel.send(get_rank("camellCase#NA1"))
 
+    elif message.content.startswith('$rank'):
+    	await message.channel.send(get_rank(message.content[6:]))
 
 client.run(token)
