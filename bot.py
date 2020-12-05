51c02ee8c9eeffeaea59ed3d7f1b938ada249a0e
Sameer Pai <sampai@mit.edu>
2020-12-05 16:19:56 -0500

    rewrote

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import discord
+from discord.ext import commands
 import requests
 import os
 import urllib
 from bs4 import BeautifulSoup
 
-token = os.getenv("simp_AI")
+bot = commands.Bot(command_prefix="$")
 
 def get_rank(name):
 	url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
@@ -17,48 +18,23 @@ def get_rank(name):
 		ans = "bad"
 	return ans
 
-
-client = discord.Client()
-camel_rank = get_rank("camellCase#NA1")
-tagdict = eval(open("tags", "r").read())
-print(tagdict)
-
-@client.event
+@bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+	print('We have logged in as {0.user}'.format(bot))
 
-@client.event
-async def on_message(message):
+@bot.command()
+async def camel(ctx):
 	global camel_rank
-	if message.author == client.user:
-		return
+	cur_rank = get_rank("camellCase#NA1")
+	await ctx.send(get_rank("camellCase#NA1"))
+	if cur_rank != camel_rank and cur_rank != "bad":
+		await ctx.send("<@&781636133762629632>, camel rank has changed to " + cur_rank + "!")
+		camel_rank = cur_rank
 
-	if message.author.id == 743713271789387877 and len(message.mentions) > 0 and message.mentions[0].id == 271689732386324482:
-		return
+@bot.command()
+async def rank(ctx):
+	await ctx.send(get_rank(ctx.message.content[6:]))
 
-	if message.content.startswith('$camel'):
-		cur_rank = get_rank("camellCase#NA1")
-		await message.channel.send(get_rank("camellCase#NA1"))
-		if cur_rank != camel_rank and cur_rank != "bad":
-			await message.channel.send("<@&781636133762629632>, camel rank has changed to " + cur_rank + "!")
-			camel_rank = cur_rank
-		
-
-	elif message.content.startswith('$rank'):
-		await message.channel.send(get_rank(message.content[6:]))
-
-	elif message.content.startswith('$userrank'):
-		if message.mentions[0].id not in tagdict:
-			await message.channel.send("bad")
-		else:
-			await message.channel.send(get_rank(tagdict[message.mentions[0].id]))
-		
-	elif message.content.startswith('$setrank'):
-		tagdict[message.author.id] = message.content[9:]
-		await message.channel.send("Rank for " + message.author.mention + " set to " + message.content[9:])
-
-	elif message.content.startswith("$export"):
-		print(tagdict)
-
-client.run(token)
-
+token = os.getenv("simp_AI")
+camel_rank = get_rank("camellCase#NA1")
+bot.run(token)
