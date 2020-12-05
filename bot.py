5999406e0e3e04f3c36b31b11d9b09407427b7e3
Sameer Pai <sampai@mit.edu>
2020-12-05 16:35:21 -0500

    made better

--- bot.py
+++ bot.py
@@ -1,40 +1,16 @@
 import discord
 from discord.ext import commands
-import requests
 import os
-import urllib
-from bs4 import BeautifulSoup
+
 
 bot = commands.Bot(command_prefix="$")
+bot.load_extension('cogs.ranks')
 
-def get_rank(name):
-	url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
-	req = requests.get(url)
-	soup = BeautifulSoup(req.content, "html.parser")
-	ans = ""
-	try:
-		ans = soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
-	except:
-		ans = "bad"
-	return ans
 
 @bot.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(bot))
 
-@bot.command()
-async def camel(ctx):
-	global camel_rank
-	cur_rank = get_rank("camellCase#NA1")
-	await ctx.send(get_rank("camellCase#NA1"))
-	if cur_rank != camel_rank and cur_rank != "bad":
-		await ctx.send("<@&781636133762629632>, camel rank has changed to " + cur_rank + "!")
-		camel_rank = cur_rank
-
-@bot.command()
-async def rank(ctx):
-	await ctx.send(get_rank(ctx.message.content[6:]))
 
 token = os.getenv("simp_AI")
-camel_rank = get_rank("camellCase#NA1")
 bot.run(token)
---
+++ cogs/ranks.py
@@ -0,0 +1,38 @@
+from discord.ext import commands
+import urllib
+from bs4 import BeautifulSoup
+import requests
+
+def get_rank(name):
+	url = "https://tracker.gg/valorant/profile/riot/" + urllib.parse.quote(name) + "/overview?playlist=competitive"
+	req = requests.get(url)
+	soup = BeautifulSoup(req.content, "html.parser")
+	ans = ""
+	try:
+		ans = soup.find_all("span", class_="valorant-highlighted-stat__value")[0].text
+	except:
+		ans = "bad"
+	return ans
+
+
+class Ranks(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.camel_rank = get_rank("camellCase#NA1")
+
+	@commands.command()
+	async def camel(self, ctx):
+		cur_rank = get_rank("camellCase#NA1")
+		await ctx.send(get_rank("camellCase#NA1"))
+		if cur_rank != self.camel_rank and cur_rank != "bad":
+			await ctx.send("<@&781636133762629632>, camel rank has changed to " + cur_rank + "!")
+			self.camel_rank = cur_rank
+
+	@commands.command()
+	async def rank(self, ctx):
+		await ctx.send(get_rank(ctx.message.content[6:]))
+
+def setup(bot):
+	bot.add_cog(Ranks(bot))
+
+
